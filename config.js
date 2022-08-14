0dd61db3f96c8151dce23eeb37f747a37239c3f6
Botwaversi1 <110661331+Botwaversi1@users.noreply.github.com>
2022-08-14 19:28:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6285702019751'],
-  ['6285702019751', 'DITZ BOT', true]
+  ['6285244925781'],
+  ['6285244925781', 'Botfayy', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
